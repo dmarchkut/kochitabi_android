69fab3a84253ebe150f54d5f1f14dbf681e77caf
gatosyocora <kut190316q@gmail.com>
2017-12-04 14:14:16 +0900

    ファイルを作成

---
+++ app/src/main/java/jp/dmarch/kochitabi/AugmentedGuideActivity.java
@@ -0,0 +1,6 @@
+package jp.dmarch.kochitabi;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AugmentedGuideActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/jp/dmarch/kochitabi/LocationAcqisition.java
@@ -0,0 +1,4 @@
+package jp.dmarch.kochitabi;
+
+public class LocationAcqisition {
+}
