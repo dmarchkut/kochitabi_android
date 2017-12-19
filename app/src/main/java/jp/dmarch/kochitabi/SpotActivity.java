d43235e289395742c2071b506c3acd3f0243395f
小笠原可偉 <Kai@ogasawarakai-no-iMac.local>
2017-12-19 20:19:48 +0900

    SpotActivity,SpotFragment更新(機能のみ稼働)

--- app/src/main/java/jp/dmarch/kochitabi/SpotActivity.java
+++ app/src/main/java/jp/dmarch/kochitabi/SpotActivity.java
@@ -1,6 +1,72 @@
 package jp.dmarch.kochitabi;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+/*
+ * http://androhi.hatenablog.com/entry/2015/06/17/083000
+ * https://qiita.com/furu8ma/items/1602a4bbed4303fec5b1
+ */
+
 public class SpotActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_spot);     // xml読み込み
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setSpotFragment();      // setSpotFragment呼び出し
+
+    }
+
+    /* SpotFragmenクラスを使用するための設定を行うメソッド */
+    private void setSpotFragment() {
+        // xml要素読み込み
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.spotTabs);
+        ViewPager viewPager = (ViewPager)findViewById(R.id.spotPager);
+
+        // タブ名称設定
+        final String[] pageTitle = {"五十音順", "近い順"};
+
+        // Fragment管理
+        FragmentManager manager = getSupportFragmentManager();
+
+        // Fragment接続
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(manager) {
+            // Fragment要素取得
+            @Override
+            public Fragment getItem(int position) {
+                return SpotFragment.newInstance(position + 1);
+            }
+
+            // タブ名称取得
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return pageTitle[position];
+            }
+
+            // タブ長さ（名称設定の個数に応じて変動）
+            @Override
+            public int getCount() {
+                return pageTitle.length;
+            }
+
+        };
+
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
 }
--- app/src/main/java/jp/dmarch/kochitabi/SpotFragment.java
+++ app/src/main/java/jp/dmarch/kochitabi/SpotFragment.java
@@ -1,4 +1,41 @@
 package jp.dmarch.kochitabi;
 
-public class SpotFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SpotFragment extends Fragment {
+
+    private static final String ARG_PARAM = "page";
+    private String mParam;
+    //private OnFragmentInteractionListener mListener;
+
+    public SpotFragment() {
+    }
+
+    public static SpotFragment newInstance(int page) {
+        SpotFragment fragment = new SpotFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM, page);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        int page = getArguments().getInt(ARG_PARAM, 0);
+        View view = inflater.inflate(R.layout.fragment_spot, container, false);
+        ((TextView)view.findViewById(R.id.textView_spotFragment)).setText("Page" + page);
+
+        return view;
+    }
+
 }
