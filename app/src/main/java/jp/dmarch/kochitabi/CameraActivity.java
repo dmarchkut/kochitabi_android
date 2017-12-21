8b38ca1d092539486e3dcc58da0da0b1c10040c7
huncro <crow417.shiina@gmail.com>
2017-12-21 22:39:36 +0900

    CameraActivityへの遷移を追加（テスト用）

--- app/src/main/java/jp/dmarch/kochitabi/CameraActivity.java
+++ app/src/main/java/jp/dmarch/kochitabi/CameraActivity.java
@@ -1,6 +1,16 @@
 package jp.dmarch.kochitabi;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+
+    }
+
 }
--- app/src/main/java/jp/dmarch/kochitabi/HomeActivity.java
+++ app/src/main/java/jp/dmarch/kochitabi/HomeActivity.java
@@ -1,7 +1,10 @@
 package jp.dmarch.kochitabi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
