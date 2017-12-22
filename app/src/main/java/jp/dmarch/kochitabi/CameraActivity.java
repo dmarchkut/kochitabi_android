6df7063f901c815eec350081875fac4e108d14ec
yum1929 <dangodaikazoku22@me.com>
2017-12-23 00:46:10 +0900

    写真撮影、共有はできそう。URI周り調べて保存やってみる

--- app/src/main/java/jp/dmarch/kochitabi/CameraActivity.java
+++ app/src/main/java/jp/dmarch/kochitabi/CameraActivity.java
@@ -29,8 +29,9 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // xmlファイルと紐付け
         setContentView(R.layout.activity_camera);
-
+        // ArchitectViewと紐付け
         this.architectView = (ArchitectView)this.findViewById( R.id.architectView );
         final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
         // ライセンスキーの読み込み
@@ -40,70 +41,90 @@ public class CameraActivity extends AppCompatActivity {
         try {
             // ArchitectViewのonCreateメソッドを実行
             this.architectView.onCreate( config );
-        } catch (RuntimeException ex)
-        {
+        } catch (RuntimeException ex) {
             this.architectView = null;
-            Toast.makeText(getApplicationContext(), "can't create Architect View", Toast.LENGTH_SHORT).show();
+            // エラー文を表示
+            Toast.makeText(getApplicationContext(), "ArchitectViewを作成できません", Toast.LENGTH_SHORT).show();
         }
-
-        Button cameraButton = findViewById(R.id.camera_button);
 
         if (savedInstanceState != null){
             cameraUri = savedInstanceState.getParcelable("CaptureUri");
         }
 
+        // カメラボタンと紐付け
+        Button cameraButton = findViewById(R.id.camera_button);
+        // カメラボタンがクリックされた時の処理
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //camera.takePicture(null, null, new TakePictureCallback());
-                CaptureScreen();
+                captureScreen();
             }
         });
     }
 
 
-    void CaptureScreen() {
-        if ("button".equalsIgnoreCase(cameraUri.getHost())) {
-            CameraActivity.this.architectView.captureScreen(ArchitectView.CaptureScreenCallback.CAPTURE_MODE_CAM_AND_WEBVIEW, new CaptureScreenCallback() {
+    private void captureScreen() {
+        CameraActivity.this.architectView.captureScreen(ArchitectView.CaptureScreenCallback.CAPTURE_MODE_CAM_AND_WEBVIEW, new CaptureScreenCallback() {
 
-                @Override
-                public void onScreenCaptured(final Bitmap screenCapture) {
-                    // store screenCapture into external cache directory
-                    final File screenCaptureFile = new File(Environment.getExternalStorageDirectory().toString(), "screenCapture_" + System.currentTimeMillis() + ".jpg");
+            @Override
+            public void onScreenCaptured(final Bitmap screenCapture) {
+                // store screenCapture into external cache directory
+                final File screenCaptureFile = new File(Environment.getExternalStorageDirectory().toString(), "screenCapture_" + System.currentTimeMillis() + ".jpg");
 
-                    // 1. Save bitmap to file & compress to jpeg. You may use PNG too
-                    try {
-                        final FileOutputStream out = new FileOutputStream(screenCaptureFile);
-                        screenCapture.compress(Bitmap.CompressFormat.JPEG, 90, out);
-                        out.flush();
-                        out.close();
+                // 1. Save bitmap to file & compress to jpeg. You may use PNG too
+                try {
+                    final FileOutputStream out = new FileOutputStream(screenCaptureFile);
+                    screenCapture.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                    out.flush();
+                    out.close();
 
-                        // 2. create send intent
-                        final Intent share = new Intent(Intent.ACTION_SEND);
-                        share.setType("image/jpg");
-                        share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(screenCaptureFile));
+                    // 2. create send intent
+                    final Intent share = new Intent(Intent.ACTION_SEND);
+                    share.setType("image/jpg");
+                    share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(screenCaptureFile));
 
-                        // 3. launch intent-chooser
-                        final String chooserTitle = "Share Snaphot";
-                        CameraActivity.this.startActivity(Intent.createChooser(share, chooserTitle));
+                    // 3. launch intent-chooser
+                    final String chooserTitle = "Share Snaphot";
+                    CameraActivity.this.startActivity(Intent.createChooser(share, chooserTitle));
 
-                    } catch (final Exception e) {
-                        // should not occur when all permissions are set
-                        CameraActivity.this.runOnUiThread(new Runnable() {
+                } catch (final Exception e) {
+                    // should not occur when all permissions are set
+                    CameraActivity.this.runOnUiThread(new Runnable() {
 
-                            @Override
-                            public void run() {
-                                // show toast message in case something went wrong
-                                Toast.makeText(CameraActivity.this, "Unexpected error, " + e.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
+                        @Override
+                        public void run() {
+                            // show toast message in case something went wrong
+                            Toast.makeText(CameraActivity.this, "Unexpected error, " + e, Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
-            });
-        }
+            }
+        });
     }
 
 
+/*
+    class TakePictureCallback implements Camera.PictureCallback {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            try {
+                File dir = new File(
+                        Environment.getExternalStorageDirectory(), "Camera");
+                if(!dir.exists()) {
+                    dir.mkdir();
+                }
+                File f = new File(dir, "img.jpg");
+                FileOutputStream fos = new FileOutputStream(f);
+                fos.write(data);
+                Toast.makeText(getApplicationContext(),
+                        "写真を保存しました", Toast.LENGTH_LONG).show();
+                fos.close();
+                camera.startPreview();
+            } catch (Exception e) { }
+        }
+    }
+*/
     @Override
     protected void onPostCreate( final Bundle savedInstanceState ) {
         super.onPostCreate(savedInstanceState);
