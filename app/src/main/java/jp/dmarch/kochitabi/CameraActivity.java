526a94730dcd3ae647de5f53eeea5c7340a1f8f2
yum1929 <dangodaikazoku22@me.com>
2017-12-23 18:42:46 +0900

    写真の保存目処が立ったのでひとまず更新

--- app/src/main/java/jp/dmarch/kochitabi/CameraActivity.java
+++ app/src/main/java/jp/dmarch/kochitabi/CameraActivity.java
@@ -7,37 +7,35 @@ import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
-
-import com.wikitude.architect.ArchitectView;
-import com.wikitude.architect.ArchitectStartupConfiguration;
-
-import com.wikitude.architect.ArchitectView.ArchitectUrlListener;
-import com.wikitude.architect.ArchitectView.CaptureScreenCallback;
 import android.net.Uri;
 import android.graphics.Bitmap;
 import android.content.Intent;
 
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+
+import com.wikitude.architect.ArchitectView;
+import com.wikitude.architect.ArchitectStartupConfiguration;
+import com.wikitude.architect.ArchitectView.CaptureScreenCallback;
+
+
+
 public class CameraActivity extends AppCompatActivity {
     private ArchitectView architectView;
-    private Uri cameraUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // xmlファイルと紐付け
         setContentView(R.layout.activity_camera);
+
         // ArchitectViewと紐付け
         this.architectView = (ArchitectView)this.findViewById( R.id.architectView );
         final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
-        // ライセンスキーの読み込み
-        config.setLicenseKey(TestWikitude.getWikitudeSDKLicenseKey());
-        // バックカメラを使用する
-        config.setCameraPosition(TestWikitude.getCameraPosition());
+        config.setLicenseKey(TestWikitude.getWikitudeSDKLicenseKey());  // ライセンスキーの読み込み
+        config.setCameraPosition(TestWikitude.getCameraPosition());     // バックカメラを使用する
         try {
             // ArchitectViewのonCreateメソッドを実行
             this.architectView.onCreate( config );
@@ -47,17 +45,12 @@ public class CameraActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "ArchitectViewを作成できません", Toast.LENGTH_SHORT).show();
         }
 
-        if (savedInstanceState != null){
-            cameraUri = savedInstanceState.getParcelable("CaptureUri");
-        }
-
         // カメラボタンと紐付け
         Button cameraButton = findViewById(R.id.camera_button);
-        // カメラボタンがクリックされた時の処理
+        /* カメラボタンがクリックされた時の処理 */
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //camera.takePicture(null, null, new TakePictureCallback());
                 captureScreen();
             }
         });
@@ -66,19 +59,30 @@ public class CameraActivity extends AppCompatActivity {
 
     private void captureScreen() {
         CameraActivity.this.architectView.captureScreen(ArchitectView.CaptureScreenCallback.CAPTURE_MODE_CAM_AND_WEBVIEW, new CaptureScreenCallback() {
-
             @Override
             public void onScreenCaptured(final Bitmap screenCapture) {
-                // store screenCapture into external cache directory
-                final File screenCaptureFile = new File(Environment.getExternalStorageDirectory().toString(), "screenCapture_" + System.currentTimeMillis() + ".jpg");
+                // 「kochitabiAR」って名前のディレクトリを作成
+                File fileName = new File(Environment.getExternalStorageDirectory(), "kochitabiAR");
+                if(!fileName.exists()) {
+                    fileName.mkdir();
+                }
+                // 作成したkochitabiARの中に「kochitabiAR_時間.jpg」って名前の写真を格納する
+                final File screenCaptureFile = new File(fileName, "kochitabiAR_" + System.currentTimeMillis() + ".jpg");
 
-                // 1. Save bitmap to file & compress to jpeg. You may use PNG too
+                /* 写真の保存処理 */
                 try {
+                    // 写真の保存処理を開始する
                     final FileOutputStream out = new FileOutputStream(screenCaptureFile);
+                    // JPEG形式度保存する
                     screenCapture.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                    // 保存完了テキストの表示
+                    Toast.makeText(getApplicationContext(), "写真を保存しました", Toast.LENGTH_LONG).show();
+                    // 保存処理終了
                     out.flush();
                     out.close();
 
+
+/*
                     // 2. create send intent
                     final Intent share = new Intent(Intent.ACTION_SEND);
                     share.setType("image/jpg");
@@ -87,44 +91,24 @@ public class CameraActivity extends AppCompatActivity {
                     // 3. launch intent-chooser
                     final String chooserTitle = "Share Snaphot";
                     CameraActivity.this.startActivity(Intent.createChooser(share, chooserTitle));
-
+*/
+                /* 全てのアクセス許可が設定されているときは実行されない部分 */
                 } catch (final Exception e) {
-                    // should not occur when all permissions are set
                     CameraActivity.this.runOnUiThread(new Runnable() {
-
                         @Override
                         public void run() {
-                            // show toast message in case something went wrong
-                            Toast.makeText(CameraActivity.this, "Unexpected error, " + e, Toast.LENGTH_LONG).show();
+                            // 誤りがある場合にエラー文を表示する
+                            Toast.makeText(CameraActivity.this, "予期せぬエラーが発生しました" + e, Toast.LENGTH_LONG).show();
                         }
                     });
                 }
+
             }
         });
     }
 
 
-/*
-    class TakePictureCallback implements Camera.PictureCallback {
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            try {
-                File dir = new File(
-                        Environment.getExternalStorageDirectory(), "Camera");
-                if(!dir.exists()) {
-                    dir.mkdir();
-                }
-                File f = new File(dir, "img.jpg");
-                FileOutputStream fos = new FileOutputStream(f);
-                fos.write(data);
-                Toast.makeText(getApplicationContext(),
-                        "写真を保存しました", Toast.LENGTH_LONG).show();
-                fos.close();
-                camera.startPreview();
-            } catch (Exception e) { }
-        }
-    }
-*/
+
     @Override
     protected void onPostCreate( final Bundle savedInstanceState ) {
         super.onPostCreate(savedInstanceState);
