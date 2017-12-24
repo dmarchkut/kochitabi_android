0f68cd24605fbf19ebab0ff6bd24896232df19c0
gatosyocora <kut190316q@gmail.com>
2017-12-24 15:32:46 +0900

    少し書き始めた

--- app/src/main/java/jp/dmarch/kochitabi/ServerExchange.java
+++ app/src/main/java/jp/dmarch/kochitabi/ServerExchange.java
@@ -1,4 +1,32 @@
 package jp.dmarch.kochitabi;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class ServerExchange {
+
+    /* サーバとの通信を開始 */
+    public void connectServer() {
+
+    }
+
+    /* サーバとの通信を終了 */
+    public void disconnectServer() {
+
+    }
+
+    /* サーバからすべてのテーブルデータを取得 */
+    public ArrayList<ArrayList<Map<String, Object>>> getLocalDataBaseOption() {
+
+    }
+
+    /* サーバからローカル環境テーブルデータを取得 */
+    public ArrayList<Map<String, Object>> getEnviornmentTable() {
+
+    }
+
+    /* サーバからローカルキャラクターテーブルデータを取得 */
+    public ArrayList<Map<String, Object>> getCharacterTable() {
+
+    }
 }
