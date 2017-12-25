8de6734fde73d657e5963c2154fd6f5f3fa148f0
yum1929 <dangodaikazoku22@me.com>
2017-12-25 18:14:30 +0900

    完成しました

--- app/src/main/java/jp/dmarch/kochitabi/SavePhotoActivity.java
+++ app/src/main/java/jp/dmarch/kochitabi/SavePhotoActivity.java
@@ -1,20 +1,19 @@
 package jp.dmarch.kochitabi;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.Intent;
+import android.content.ContentValues;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.content.Intent;
-import android.widget.Toast;
+import android.provider.MediaStore;
 import android.net.Uri;
 
 import java.io.File;
-import java.io.FileOutputStream;
-
 
 public class SavePhotoActivity extends AppCompatActivity {
 
@@ -29,7 +28,7 @@ public class SavePhotoActivity extends AppCompatActivity {
         Bundle bundle = intent.getExtras();
         final File screenCaptureFile = (File)bundle.get("bitmapData");
         // 写真ファイルパスを取得
-        String filePath = screenCaptureFile.toString();
+        final String filePath = screenCaptureFile.toString();
 
         // imageViewと紐付け
         ImageView imageView = findViewById(R.id.image_view);
@@ -48,8 +47,11 @@ public class SavePhotoActivity extends AppCompatActivity {
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                deletePhoto();
-
+                // 写真の削除を行う
+                screenCaptureFile.delete();
+                // 画面遷移の処理
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);  // 切り替え準備
+                startActivity(intent);                                                      // Activity切り替え
             }
         });
 
@@ -59,7 +61,11 @@ public class SavePhotoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 /// 保存完了テキストの表示
                 Toast.makeText(getApplicationContext(), "写真を保存しました", Toast.LENGTH_LONG).show();
-
+                // 写真をギャラリーに保存する
+                addImageToGallery(filePath);
+                // 画面遷移の処理
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);  // 切り替え準備
+                startActivity(intent);
             }
         });
 
@@ -70,27 +76,33 @@ public class SavePhotoActivity extends AppCompatActivity {
                 sharePhoto(screenCaptureFile);
             }
         });
-
-
     }
 
-
-    private void deletePhoto() {
-
-    }
-
+    /* 写真共有を行うためのメソッド */
     private void sharePhoto(File screenCaptureFile) {
-        // 2. create send intent
+        // 共有するものを設定する
         final Intent share = new Intent(Intent.ACTION_SEND);
         share.setType("image/jpg");
         share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(screenCaptureFile));
 
-        // 3. launch intent-chooser
-        final String chooserTitle = "Share Snaphot";
+        // 写真共有画面を起動する
+        final String chooserTitle = "写真共有";
         SavePhotoActivity.this.startActivity(Intent.createChooser(share, chooserTitle));
     }
 
+    /* 写真をギャラリーに保存するためのメソッド */
+    private void addImageToGallery(String filePath) {
+        try {
+            ContentValues values = new ContentValues();
 
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            values.put(MediaStore.MediaColumns.DATA, filePath);
 
-
+            getApplicationContext().getContentResolver()
+                    .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
