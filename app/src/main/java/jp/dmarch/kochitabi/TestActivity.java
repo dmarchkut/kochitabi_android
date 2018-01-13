6d1c251c0b8aa2a8cba1543c567044b3139349c1
gatosyocora <kut190316q@gmail.com>
2018-01-13 23:31:02 +0900

    テストコードを変更

--- app/src/main/java/jp/dmarch/kochitabi/TestActivity.java
+++ app/src/main/java/jp/dmarch/kochitabi/TestActivity.java
@@ -19,7 +19,7 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test2);
+        setContentView(R.layout.activity_test);
     }
 
     @Override
